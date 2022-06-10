27e26743b46b0f4e0b959c3c5d4306146c05369b
hiro961227 <67949100+hiro961227@users.noreply.github.com>
2022-06-10 18:14:52 +0900

    220610

--- index2.js
+++ index2.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 		$("#"+tab_id).addClass('openPage');
 	})
 
-	//wheel event
+	//about_wheel event
 	window.addEventListener("wheel", function(e){
 		e.preventDefault();
 	},{passive : false});
@@ -22,10 +22,40 @@ $(document).ready(function(){
 		var lastPage = $("#aboutMe article").length;
 
 		$html.animate({scrollTop:0},10);
-		$('.content_main').on("wheel", function(e){
-
+		$('.content_main #aboutMe').on("wheel", function(e){
 			if($html.is(":animated")) return;
 			if(e.originalEvent.deltaY > 0){
+				if($("#section3")){
+					if(!$('.s03').hasClass('circle_delay')){
+						setTimeout(() => {
+							//skill circle
+								draw(85,'.pie-chart1','#7887D1');
+								draw(80,'.pie-chart2','#7887D1');
+								draw(70,'.pie-chart3','#7887D1');
+								draw(70,'.pie-chart4','#7887D1');
+
+								function draw(max, classname, colorname){
+									var i=1;
+										var func1 = setInterval(function(){
+												if(i<max){
+																color1(i,classname,colorname);
+																i++;
+												} else{
+														clearInterval(func1);
+												}
+										},10);
+								}
+								function color1(i, classname,colorname){
+									$(classname).css({
+														"background":"conic-gradient("+colorname+" 0% "+i+"%, rgba(120,135,209,.12) "+i+"% 100%)"
+									});
+								}
+						}, 500);
+					}else if($('#section4')){
+						$('.s03').addClass('circle_delay');
+					}
+				}
+
 				if(page== lastPage) return;
 				page++;
 			}else if(e.originalEvent.deltaY < 0){
@@ -37,6 +67,17 @@ $(document).ready(function(){
 			$html.animate({scrollTop : posTop});
 
 		});
+
+	//project_wheel event
+	$(".content_main #project").on('mousewheel',function(e){
+		var wheelDelta = e.originalEvent.wheelDelta;
+		if(wheelDelta > 0){
+			console.log("up");
+			$(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+		}else{
+				console.log("down");
+					$(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+		}});
 });
 
 // morphing settings
@@ -65,3 +106,5 @@ $(function() {
 					fps: 50
 	});
 });
+
+//fadeIn
